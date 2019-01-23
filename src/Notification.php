b8c7cac769b3ae5722630d2e19dc1c88e64e0e9d
Janne Aalto <janne.aalto@frantic.com>
2019-01-23 09:32:53 +0200

    initial

---
+++ src/Notification.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janneaalto
+ * Date: 18/06/2018
+ * Time: 13.17
+ */
+
+namespace Frc\Slack;
+
+class Notification {
+
+    private $webhookUrl = '';
+    private $slackReportChannel = '';
+
+    protected static $instance = null;
+
+    public static function getInstance() {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct() {
+        $this->webhookUrl = getenv('SLACK_WEBHOOK_URL');
+        $this->slackReportChannel = getenv('SLACK_REPORT_CHANNEL');
+    }
+
+    private function checkEnv() {
+        if ($this->webhookUrl == false || $this->slackReportChannel == false) {
+            return false;
+        }
+        return true;
+    }
+
+    public function sendMessageToSlack($message) {
+
+        if (!$this->checkEnv()) {
+            return false;
+        }
+
+        $data = 'payload=' . json_encode([
+                'channel'    => $this->slackReportChannel,
+                'username'   => 'PHP Notifier',
+                'text'       => $message,
+                'icon_emoji' => ':female-detective:'
+            ]);
+
+        $ch = curl_init($this->webhookUrl);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+}
